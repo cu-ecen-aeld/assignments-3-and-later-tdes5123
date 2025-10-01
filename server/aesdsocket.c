fa1a1c1b98a01ea3c8d7cda758842b8f777b75fd
Tyler Deschamps <tdes5123@gmail.com>
2025-09-30 19:03:22 -0600

    successfully completed full-test

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,14 +1,17 @@
-#include <arpa/inet.h>
 #include <stdio.h>
+#include <errno.h>
 #include <syslog.h>
+#include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <errno.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
 
 #define PORT 9000
 #define BUFF_SZ 1024
+
+volatile int serverRunning = 1;
 
 void createSockDataFile() 
 {
@@ -22,21 +25,14 @@ void createSockDataFile()
             mkdir("/var/tmp", 0777);
         }
     }
-
-    // Create or truncate the file
-    fp = fopen("/var/tmp/aesdsocketdata", "w");
-    if (fp == NULL) {
-        perror("File creation failed");
-        exit(1);
-    }
-    fclose(fp);
 }
 
-int main(int argc, char *argv[]) 
+int acceptConnection(int daemonMode)
 {
     int sock, clientSock;
     struct sockaddr_in addr, remoteAddr;
     char buff[BUFF_SZ] = {0};
+    size_t charsAdded = 0;
 
     // Create the in socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -57,72 +53,123 @@ int main(int argc, char *argv[])
         perror("bind failed");
     }
 
-    // Attempt to listen for incoming connections
-    if(listen(sock, 5) < 0) {
-        perror("listen failed");
-        close(sock);
-        return 1;
-    }
-
-    clientSock = accept(sock, (struct sockaddr*)&addr, (socklen_t*)&addr);
-    if(clientSock < 0) {
-        perror("accept failed");
-        close(sock);
-        return 1;
-    }
-
-    // Log that the connection has been made
-    socklen_t addr_len = sizeof(remoteAddr);
-    if(getpeername(clientSock, (struct sockaddr*)&remoteAddr, &addr_len) == -1) {
-        perror("getpeername failed");
+    // Daemonize if needed
+    if (daemonMode && daemon(0, 0) < 0) {
+        perror("daemon");
         close(clientSock);
-        close(sock);
-        return 1;
+        exit(EXIT_FAILURE);
     }
 
-    char remoteIp[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &remoteAddr.sin_addr, remoteIp, sizeof(remoteIp));
-    openlog("aesdsocket", LOG_PID, LOG_USER);
-    syslog(LOG_INFO, "Accepted connection from %s", remoteIp);
-    closelog();
-    createSockDataFile();
-    FILE *fp = fopen("/var/tmp/aesdsocketdata", "a");
-    
-    // Receive data while there's data to read -
-    while (1) {
-        int rd = recv(clientSock, buff, BUFF_SZ, 0);
-        if (rd < 0) {
-            perror("recv failed");
+    while(serverRunning) {
+        // Attempt to listen for incoming connections
+        if(listen(sock, 5) < 0) {
+            perror("listen failed");
+            close(sock);
+            return 1;
+        }
+
+        // Accept a connection
+        clientSock = accept(sock, (struct sockaddr*)&addr, (socklen_t*)&addr);
+        if(clientSock < 0) {
+            perror("accept failed");
             close(clientSock);
             return 1;
-        } else if (rd == 0) {
-            // Connection closed
-            break;
-        } else {
-            // Write to file
-            fwrite(buff, sizeof(char), rd, fp);
-            // Check for newline to end
-            if (buff[rd - 1] == '\n') {
+        }
+
+        // Log that the connection has been made
+        socklen_t addr_len = sizeof(remoteAddr);
+        if(getpeername(clientSock, (struct sockaddr*)&remoteAddr, &addr_len) == -1) {
+            perror("getpeername failed");
+            close(clientSock);
+            close(sock);
+            return 1;
+        }
+
+        char remoteIp[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &remoteAddr.sin_addr, remoteIp, sizeof(remoteIp));
+        openlog("aesdsocket", LOG_PID, LOG_USER);
+        syslog(LOG_INFO, "Accepted connection from %s", remoteIp);
+        closelog();
+        createSockDataFile();
+        FILE *fp = fopen("/var/tmp/aesdsocketdata", "a+");
+        
+        // Receive data while there's data to read -
+        while (1) {
+            int rd = recv(clientSock, buff, BUFF_SZ, 0);
+            if (rd < 0) {
+                perror("recv failed");
+                close(clientSock);
+                return 1;
+            } else if (rd == 0) {
+                // Connection closed
                 break;
+            } else {
+                // Write to file
+                fwrite(buff, sizeof(char), rd, fp);
+                charsAdded += rd;
+                printf("Received %d bytes: %.*s", rd, rd, buff);
+                // Check for newline to end
+                if (buff[rd - 1] == '\n') {
+                    break;
+                }
             }
         }
-    }
 
-    // Send back the file contents
-    fseek(fp, 0, SEEK_SET);
-    while (fgets(buff, BUFF_SZ, fp) != NULL) {
-        send(clientSock, buff, strlen(buff), 0);
+        // Send back the file contents
+        fseek(fp, 0, SEEK_SET);
+        // fseek(fp, -charsAdded, SEEK_END);
+        while (fgets(buff, BUFF_SZ, fp) != NULL) {
+            printf("sending: %s", buff);
+            // send(clientSock, buff, BUFF_SZ, 0);
+            send(clientSock, buff, strlen(buff), 0);
+        }
+
+        fclose(fp);
+        close(clientSock);
+
+        // Log that the connection has been closed
+        openlog("aesdsocket", LOG_PID, LOG_USER);
+        syslog(LOG_INFO, "Closed connection from %s", remoteIp);
+        closelog();
     }
 
     // Close connection
-    fclose(fp);
-    close(clientSock);
     close(sock);
+}
 
-    // Log that the connection has been closed
+// To be used as the signal handler for SIGINT and SIGTERM 
+void terminateGracefully(int signum) 
+{
+    // Log that the signal was caught
     openlog("aesdsocket", LOG_PID, LOG_USER);
-    syslog(LOG_INFO, "Closed connection from %s", remoteIp);
+    syslog(LOG_INFO, "Caught signal, exiting");
     closelog();
+
+    // Set the running flag to false and clean up
+    serverRunning = 0;
+}
+
+int main(int argc, char *argv[]) 
+{
+    printf("main1\n");
+    // Make sure that external signals to terminate are handled gracefully
+    struct sigaction gracefulTerm;
+    memset(&gracefulTerm, 0, sizeof(gracefulTerm));
+    gracefulTerm.sa_handler = terminateGracefully;
+    sigaction(SIGINT, &gracefulTerm, NULL);
+    sigaction(SIGTERM, &gracefulTerm, NULL);
+
+    printf("main2\n");
+
+    // Continually accept connections
+    if(argc > 1 && strcmp(argv[1], "-d") == 0) {
+        (void) acceptConnection(1);
+    } else {
+        (void) acceptConnection(0);
+    }
+        
+    remove("/var/tmp/aesdsocketdata");
+    printf("main3\n");
 
     return 0;
 }
